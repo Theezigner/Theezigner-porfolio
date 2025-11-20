5eb5d1c63948f96a4489b2b450dfc51bc32da983
Temitayo Adebayo <adebayotemitayo86@gmail.com>
2025-11-20 11:28:17 -0500

    corrected hamburger

--- src/components/Hamburger.vue
+++ src/components/Hamburger.vue
@@ -17,22 +17,22 @@ function hamburger() {
     >
       <span
         :class="isOpen ? 'rotate-50 translate-y-2' : ''"
-        class="block h-0.5 w-full bg-white rounded transform transition duration-300 ease-in-out"
+        class="block h-0.5 w-full bg-white rounded transform transition duration-200 ease-in-out"
       ></span>
       <span
         :class="isOpen ? 'opacity-0' : 'opacity-100'"
-        class="block h-0.5 w-full bg-white rounded transform transition-opacity duration-300 ease-in-out"
+        class="block h-0.5 w-full bg-white rounded transform transition-opacity duration-200 ease-in-out"
       ></span>
       <span
         :class="isOpen ? '-rotate-50 -translate-y-1' : ''"
-        class="block h-0.5 w-full bg-white rounded transform transition duration-300 ease-in-out"
+        class="block h-0.5 w-full bg-white rounded transform transition duration-200 ease-in-out"
       ></span>
     </button>
 
     <aside class="md:hidden">
       <nav
         v-if="isOpen"
-        class="absolute top-16 right-5 left-5 bg-[#0D0C21] border border-gray-800 p-5 rounded shadow-md flex flex-col gap-5"
+        class="absolute top-16 right-5 left-5 bg-[#0D0C21] border border-gray-800 p-5 rounded shadow-md flex flex-col gap-5 text-center"
       >
         <a href="#aboutme">About Me</a>
         <a href="#skills&tools">Skills & Tools</a>
